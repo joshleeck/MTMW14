0544f771c52e884a29a1045afd9f311808872998
joshleeck <32456936+joshleeck@users.noreply.github.com>
2018-03-20 22:36:59 +0800

    v1

--- Project 2/main.py
+++ Project 2/main.py
@@ -8,7 +8,7 @@ import plotting as pt
 
 #if task a = true do this, if task b = true do this etc. main(TaskA=True,TaskB=True...)
 #initialise arrays with initial conditions
-def main(TaskB = False, TaskC = False, TaskD = False):
+def main(TaskB = False, TaskC1 = False, TaskC2 = False, TaskD = False, TaskD1 = False, TaskD2 = False):
     u_on_u, v_on_v, n_on_n, u_on_n, v_on_n, v_on_u, u_on_v = bc.ic()
 
     E_num = []
@@ -23,8 +23,8 @@ def main(TaskB = False, TaskC = False, TaskD = False):
         sch.inter_v_on_n(n_on_n, v_on_v, v_on_n)
         #calculate energy on n grid
         E_num.append(sch.calc_E(n_on_n, u_on_n, v_on_n))
-        if timestep%100 == 0:
-            print timestep
+        #if timestep%100 == 0:
+        #    print timestep
 
     v_on_u = sch.inter_v_on_u(u_on_u, v_on_v, v_on_u)
     u_on_v = sch.inter_u_on_v(u_on_u, v_on_v, u_on_v)
@@ -42,10 +42,10 @@ def main(TaskB = False, TaskC = False, TaskD = False):
 
         pt.plot_TaskB(u_on_vsouth, v_on_uwest, n_on_nmiddle, n_on_n)
 
-
-    if TaskC == True:
-        pt.plot_TaskC(E_num)
-
+    if TaskC1 == True:
+        pt.plot_TaskC(E_num, 24, hours=True)
+    if TaskC2 == True:
+        pt.plot_TaskC(E_num, 576, days=True)
     # print u_on_u
     # print n_on_n
     # print v_on_v
@@ -53,17 +53,76 @@ def main(TaskB = False, TaskC = False, TaskD = False):
 
     n_on_n_ana, u_on_n_ana, v_on_n_ana = bc.ic()[2:5]
     sch.analytic_solution(n_on_n_ana, u_on_n_ana, v_on_n_ana, n_on_n[(pm.pm['nx']-1)/2,0])
-    # print u_on_n_ana
-    # print v_on_n_ana
-    # print n_on_n_ana
+    #print u_on_n_ana
+    #print v_on_n_ana
+    #print n_on_n_ana
 
     u_diff_on_n = np.subtract(u_on_n, u_on_n_ana)
     v_diff_on_n = np.subtract(v_on_n, v_on_n_ana)
     n_diff_on_n = np.subtract(n_on_n, n_on_n_ana)
 
+    if TaskD1 == True:
+        print "The energy calculated from the difference fields is " \
+              + str(sch.calc_E(n_diff_on_n, u_diff_on_n, v_diff_on_n)) + " J"
     if TaskD == True:
         pt.plot_TaskD(n_on_n_ana, n_diff_on_n)
+    if TaskD2 == True:
+        pt.plot_TaskD2(40000,25000,20000,3.22*1e13,2.33*1e13,2.12*1e13)
 
-        print sch.calc_E(n_diff_on_n, u_diff_on_n, v_diff_on_n)
+####################################################
 
-main(TaskD = True)
+def main2(TaskE1 = False, TaskE2 = False):
+    u_on_u, v_on_v, n_on_n, u_on_n, v_on_n, v_on_u, u_on_v = bc.ic()
+    u_on_u_old, v_on_v_old, n_on_n_old, u_on_n_old, v_on_n_old, v_on_u_old, u_on_v_old = bc.ic()
+    u_on_u_new, v_on_v_new, n_on_n_new, u_on_n_new, v_on_n_new, v_on_u_new, u_on_v_new = bc.ic()
+    for timestep in range(pm.pm['nt']):
+        if timestep % 2 != 0:
+            sch.SL_fbts_odd(n_on_n_new, v_on_v_new, u_on_u_new, n_on_n, u_on_u, v_on_v, \
+                            u_on_u_old, v_on_v_old, u_on_n, v_on_n, v_on_n_old, u_on_n_old)
+            u_on_n_old = copy.copy(u_on_n)
+            v_on_n_old = copy.copy(v_on_n)
+            u_on_u_old = copy.copy(u_on_u)
+            v_on_v_old = copy.copy(v_on_v)
+            n_on_n = copy.copy(n_on_n_new)
+            v_on_v = copy.copy(v_on_v_new)
+            u_on_u = copy.copy(u_on_u_new)
+            sch.inter_u_on_n(n_on_n_new, u_on_u_new, u_on_n)
+            sch.inter_v_on_n(n_on_n_new, v_on_v_new, v_on_n)
+        elif timestep % 2 == 0:
+            sch.SL_fbts_even(n_on_n_new, v_on_v_new, u_on_u_new, n_on_n, u_on_u, v_on_v, \
+                             u_on_u_old, v_on_v_old, u_on_n, v_on_n, v_on_n_old, u_on_n_old)
+            u_on_n_old = copy.copy(u_on_n)
+            v_on_n_old = copy.copy(v_on_n)
+            u_on_u_old = copy.copy(u_on_u)
+            v_on_v_old = copy.copy(v_on_v)
+            n_on_n = copy.copy(n_on_n_new)
+            v_on_v = copy.copy(v_on_v_new)
+            u_on_u = copy.copy(u_on_u_new)
+            sch.inter_u_on_n(n_on_n_new, u_on_u_new, u_on_n)
+            sch.inter_v_on_n(n_on_n_new, v_on_v_new, v_on_n)
+        print timestep
+
+    v_on_u = sch.inter_v_on_u(u_on_u, v_on_v, v_on_u)
+    u_on_v = sch.inter_u_on_v(u_on_u, v_on_v, u_on_v)
+
+    if TaskE1 == True:
+        u_on_vsouth = u_on_v[0,:].tolist()
+        u_on_vsouth.insert(0,0)
+        u_on_vsouth.append(0)
+
+        v_on_uwest = v_on_u[:,0].tolist()
+        v_on_uwest.insert(0,0)
+        v_on_uwest.append(0)
+
+        n_on_nmiddle = n_on_n[(pm.pm['nx']-1)/2,:].tolist()
+
+        pt.plot_TaskE1(u_on_vsouth, v_on_uwest, n_on_nmiddle, n_on_n)
+
+    n_on_n_ana, u_on_n_ana, v_on_n_ana = bc.ic()[2:5]
+    sch.analytic_solution(n_on_n_ana, u_on_n_ana, v_on_n_ana, n_on_n[(pm.pm['nx']-1)/2,0])
+
+    if TaskE2 == True:
+        pt.plot_TaskE2(n_on_n, n_on_n_ana)
+
+
+#main(TaskD1 = True, TaskD = True)
--- Project 2/plotting.py
+++ Project 2/plotting.py
@@ -1,8 +1,78 @@
 import matplotlib.pyplot as plt
 import parameters as pm
-
+import numpy as np
 
 def plot_TaskB(u_on_vsouth, v_on_uwest, n_on_nmiddle, n_on_n):
+    x_array = [(0.5+i)*pm.pm['d'] for i in range(pm.pm['nx']-1)]
+    x_array.insert(0,0)
+    x_array.append(pm.pm['L'])
+    y_array = [(0.5+i)*pm.pm['d'] for i in range(pm.pm['nx']-1)]
+    y_array.insert(0,0)
+    y_array.append(pm.pm['L'])
+
+    plt.subplot(1,2,1)
+    plt.title("(a)")
+    plt.plot(x_array,u_on_vsouth, label="$u$ on southern edge")
+    plt.plot(y_array, v_on_uwest, label="$v$ on western edge")
+    plt.xticks(rotation=90)
+    plt.xlabel("Spatial distance ($m$)")
+    plt.ylabel("$u$, $v$ and $\eta$ ($ms^{-1}$ and $m$)")
+    x_array = [(0.5 + i)*pm.pm['d'] for i in range(pm.pm['nx']-1)]
+    plt.plot(x_array, n_on_nmiddle, label="$\eta$ through middle")
+    plt.legend(loc='upper right', bbox_to_anchor=(0.8, -0.4))
+    plt.tight_layout()
+
+    plt.subplot(1,2,2)
+    plt.title("(b)")
+    plt.xlabel("Spatial distance ($m$)")
+    plt.xticks([0, 5, 10, 15, 20, 25],[0,200000,400000,600000,800000,1000000], rotation=90)
+    #plt.xlim(0,50)
+    #plt.ylim(0,50)
+    plt.ylabel("Spatial distance ($m$)")
+    plt.yticks([0, 5, 10, 15, 20, 25],[0,200000,400000,600000,800000,1000000])
+    plt.contourf(n_on_n)
+    plt.colorbar(label="$\eta$ (m)")
+    plt.tight_layout()
+
+def plot_TaskC(E_num, tick_space, days = False, hours = False):
+    t_array = range(pm.pm['nt'])
+    plt.plot(t_array, E_num)
+    plt.xlabel("Time (s)")
+    if hours == True:
+        plt.xlabel("Time (hours)")
+        plt.xticks(np.arange(0, pm.pm['nt'], tick_space), np.arange(0, 24), rotation=90)
+    if days == True:
+        plt.xlabel("Time (days)")
+        plt.xticks(np.arange(0,pm.pm['nt'],tick_space),np.arange(0, 30),rotation=90)
+    plt.ylabel("$E$ (J)")
+
+def plot_TaskD(n_on_n_ana, n_diff_on_n):
+    plt.subplot(1,2,1)
+    plt.title("a")
+    plt.xlabel("Spatial distance ($m$)")
+    plt.xticks([0, 5, 10, 15, 20, 25],[0,200000,400000,600000,800000,1000000], rotation=90)
+    plt.ylabel("Spatial distance ($m$)")
+    plt.yticks([0, 5, 10, 15, 20, 25],[0,200000,400000,600000,800000,1000000])
+    plt.contourf(n_on_n_ana)
+    plt.colorbar(label="$\eta$ (m)")
+    plt.tight_layout()
+
+    plt.subplot(1,2,2)
+    plt.title("b")
+    plt.xlabel("Spatial distance ($m$)")
+    plt.xticks([0, 5, 10, 15, 20, 25],[0,200000,400000,600000,800000,1000000], rotation=90)
+    plt.ylabel("Spatial distance ($m$)")
+    plt.yticks([0, 5, 10, 15, 20, 25],[0,200000,400000,600000,800000,1000000])
+    plt.contourf(n_diff_on_n)
+    plt.colorbar(label="Difference in $\eta$ (m)")
+    plt.tight_layout()
+
+def plot_TaskD2(r1, r2, r3, error1, error2, error3):
+    plt.xlabel("Horizontal resolution (m)")
+    plt.ylabel("Error in model (J)")
+    plt.plot([r1,r2,r3],[error1,error2,error3])
+
+def plot_TaskE1(u_on_vsouth, v_on_uwest, n_on_nmiddle, n_on_n):
     x_array = [(0.5+i)*pm.pm['d'] for i in range(pm.pm['nx']-1)]
     x_array.insert(0,0)
     x_array.append(pm.pm['L'])
@@ -15,7 +85,7 @@ def plot_TaskB(u_on_vsouth, v_on_uwest, n_on_nmiddle, n_on_n):
     plt.plot(x_array,u_on_vsouth)
     plt.plot(y_array, v_on_uwest)
     plt.xticks(rotation=90)
-    plt.xlabel("spatial distance ($m$)")
+    plt.xlabel("Spatial distance ($m$)")
     plt.ylabel("$u$, $v$ and $\eta$ ($ms^{-1}$ and $m$)")
     x_array = [(0.5 + i)*pm.pm['d'] for i in range(pm.pm['nx']-1)]
     plt.plot(x_array, n_on_nmiddle)
@@ -25,17 +95,15 @@ def plot_TaskB(u_on_vsouth, v_on_uwest, n_on_nmiddle, n_on_n):
     plt.title("(b)")
     plt.contourf(n_on_n)
     plt.colorbar()
+    plt.show()
 
-    t_array = range(pm.pm['nt'])
-    plt.plot(t_array, E_num)
-
-def plot_TaskD(n_on_n_ana, n_diff_on_n):
+def plot_TaskE2(n_on_n, n_on_n_ana):
     plt.subplot(1,2,1)
     plt.title("a")
-    plt.contourf(n_on_n_ana)
+    plt.contourf(n_on_n)
     plt.colorbar()
 
     plt.subplot(1,2,2)
     plt.title("b")
-    plt.contourf(n_diff_on_n)
+    plt.contourf(n_on_n_ana)
     plt.colorbar()
